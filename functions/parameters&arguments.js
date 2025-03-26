76def54f6b95cac2a6965921e62d093f836e7edb
Sophia-Hammond <beautyinbooty@gmail.com>
2025-03-26 20:57:45 +0000

    Function study default parameters

---
+++ functions/parameters&arguments.js
@@ -0,0 +1,11 @@
+function sayThanks(name) {
+    console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+  };
+  
+  sayThanks('Cole');
+
+  function daysOfTheWeek(day) {
+    console.log('Today is ' + day + ' Have a great day!');
+  };
+
+  daysOfTheWeek('Monday');
