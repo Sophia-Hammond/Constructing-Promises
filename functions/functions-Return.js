0159dc938789aa04a69711129171fb929460b88c
Sophia-Hammond <beautyinbooty@gmail.com>
2025-03-26 21:30:47 +0000

    helper functions

--- functions/functions-Return.js
+++ functions/functions-Return.js
@@ -25,4 +25,31 @@ function rectangleArea(width, height) {
 
  console.log(newProjectMass);
 
+ // helper function
+
+ function multiplyByNineFifths(number) {
+    return number * (9/5);
+  };
+  
+  function getFahrenheit(celsius) {
+    return multiplyByNineFifths(celsius) + 32;
+  };
+  
+  getFahrenheit(15); // Returns 59
+  
+
+  
+  
+  function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors(rows, columns) {
+    return monitorCount(rows, columns) * 200
+  };
+  
+  const totalCost = costOfMonitors(5, 4);
+  
+  console.log(totalCost);
+
 
