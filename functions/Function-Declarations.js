7ab6f390cfe2055ea35a061f37f30b2883c91e3b
Sophia-Hammond <beautyinbooty@gmail.com>
2025-03-26 20:33:23 +0000

    Function Declaration practise

---
+++ functions/Function-Declarations.js
@@ -0,0 +1,18 @@
+function getReminder() {
+    console.log('Water the plants.')
+  };
+
+  getReminder();
+  
+  
+  function greetInSpanish() {
+    console.log('Buenas tardes.')
+  };
+
+  greetInSpanish();
+
+  function successfulPayment() {
+    console.log('Your payment was successful!')
+  };
+
+  successfulPayment();
